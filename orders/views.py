dd924f7b84cd704b34cad41dea782883e0de6621
Ruben Tura <rubentura@proton.me>
2024-03-23 23:02:27 +0100

    added form to add new orders

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from .models import Order, Entry
+from .forms import OrderForm
 
 # Create your views here.
 def orders(request):
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = OrderForm()
+
     orders = Order.objects.all()
     entries = Entry.objects.all()
-    return render(request, 'orders.html', context={'orders': orders, 'entries': entries})
+    context = {
+        'orders': orders, 
+        'entries': entries,
+        'form': form,
+    }
+    return render(request, 'orders.html', context=context)
