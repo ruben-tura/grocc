cfc88aa397fabb0a8914690df6bd3d8a1d9ba0aa
Ruben Tura <rubentura@proton.me>
2024-03-26 09:18:01 +0100

    better logout

--- grocc/settings.py
+++ grocc/settings.py
@@ -133,3 +133,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
